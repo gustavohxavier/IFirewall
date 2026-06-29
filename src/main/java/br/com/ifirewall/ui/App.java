e4569121bff75b48bb031861d6f9adc9bcca0cc9
Gustavo Hidalgo Xavier <guhidalgox@gmail.com>
2026-06-28 23:31:24 -0300

    chore: ajuste de cores

--- src/main/java/br/com/ifirewall/ui/App.java
+++ src/main/java/br/com/ifirewall/ui/App.java
@@ -14,8 +14,9 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("MainView.fxml"));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 1024, 768);
-        stage.setTitle("Iptables Visual Builder");
+        Scene scene = new Scene(root, 1280, 800);
+        scene.getStylesheets().add(App.class.getResource("styles.css").toExternalForm());
+        stage.setTitle("IFirewall – Gerador de regras de filtragem");
         stage.setScene(scene);
         stage.show();
     }
